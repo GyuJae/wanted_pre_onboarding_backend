e129087c7c7e6d801af739a65818bd021b00934f
Gyujae <wjdrbwo1206@naver.com>
2022-04-05 00:31:00 +0900

    product model

--- products/models.py
+++ products/models.py
@@ -5,4 +5,12 @@ class Product(core_models.TimeStampedModel):
 
     """ Product Model Definition """
 
-    pass
+    title = models.CharField(max_length=140)
+    publisher_name = models.CharField(max_length=140)
+    description = models.TextField()
+    target_amount = models.IntegerField(editable=False)
+    funding_end_date = models.DateField()
+    one_time_funding_amount = models.IntegerField()
+
+    def __str__(self):
+        return self.title
